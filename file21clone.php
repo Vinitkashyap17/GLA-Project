1204c56e20e56781c4d9ce46f554893c7e7bdfb3
Vinitkashyap17 <159765839+Vinitkashyap17@users.noreply.github.com>
2024-02-13 11:19:41 +0530

    Update file21clone.php

--- file21clone.php
+++ file21clone.php
@@ -18,7 +18,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">About us</a>
+          <a class="nav-link active" aria-current="page" href="http://localhost/vinit/about.php">About us</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Contact</a>
@@ -101,7 +101,7 @@
           <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
           <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
             Continue reading
-            <img src="https://www.gla.ac.in/images/h-recc5.webp"  width="200" height="250" alt="">
+            <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
           </a>
         </div>
         <div class="col-auto d-none d-lg-block">
